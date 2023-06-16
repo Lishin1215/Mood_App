465b7af49fbbe51ae4519d7a9e6f51357d927a3d
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-06-16 11:59:51 +0800

    hide the tabBar below when entering newPage

--- MoodApp/MoodApp/ViewController/NewPageViewController.swift
+++ MoodApp/MoodApp/ViewController/NewPageViewController.swift
@@ -12,6 +12,21 @@ class NewPageViewController: UIViewController {
     //傳過來的日期
     var dateComponents: DateComponents?
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //替換自己的返回鍵（黑）
+        
+        //hide tabBar
+        tabBarController?.tabBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        //tabBar appear again
+        tabBarController?.tabBar.isHidden = false
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
