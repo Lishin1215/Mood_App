5e2a028dab4c2e02b2e706e516ae413b1d22b130
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-06-22 17:55:54 +0800

    add kingFisherWrapper extenstion

---
+++ MoodApp/MoodApp/Extensions/KingFisherWrapper.swift
@@ -0,0 +1,21 @@
+//
+//  KingFisherWrapper.swift
+//  MoodApp
+//
+//  Created by 簡莉芯 on 2023/6/22.
+//
+
+import UIKit
+import Kingfisher
+
+
+// 定義一個 UIImageView 的 extension
+extension UIImageView {
+    
+    // 定義一個設置圖片的方法，這裡使用了 Kingfisher 框架來下載圖片並緩存
+    func addImage(with url: URL?) {
+        // 使用 Kingfisher 提供的方法來下載圖片
+        self.kf.setImage(with: url)
+    }
+    
+}
