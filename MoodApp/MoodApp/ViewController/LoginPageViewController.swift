ecdd6335df4cb88df6f8734b32f4654d9a283cfd
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-06-26 22:02:22 +0800

    create LoginPageVC and add simple UI on storyboard

---
+++ MoodApp/MoodApp/ViewController/LoginPageViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginPageViewController.swift
+//  MoodApp
+//
+//  Created by 簡莉芯 on 2023/6/26.
+//
+
+import UIKit
+
+class LoginPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
