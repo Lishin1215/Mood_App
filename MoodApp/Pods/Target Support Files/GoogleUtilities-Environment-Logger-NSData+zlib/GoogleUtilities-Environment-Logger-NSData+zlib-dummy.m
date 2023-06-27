8737a30804f20e482a85cc27dc26b875c9422800
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-06-28 04:15:58 +0800

    fix issues to make archive work

--- MoodApp/Pods/Target Support Files/GoogleUtilities-Environment-Logger-NSData+zlib/GoogleUtilities-Environment-Logger-NSData+zlib-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GoogleUtilities_Environment_Logger_NSData_zlib : NSObject
-@end
-@implementation PodsDummy_GoogleUtilities_Environment_Logger_NSData_zlib
-@end
