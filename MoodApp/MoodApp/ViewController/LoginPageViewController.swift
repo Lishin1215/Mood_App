8b1315202e99f9a1aa9cf710d65cd2753c9ce3a3
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-06-28 15:55:29 +0800

    add appleLoginButton and basic function to deal with login situation (but not done yet)

--- MoodApp/MoodApp/ViewController/LoginPageViewController.swift
+++ MoodApp/MoodApp/ViewController/LoginPageViewController.swift
@@ -6,24 +6,99 @@
 //
 
 import UIKit
+import AuthenticationServices // Sign in with Apple 的主體框架
 
 class LoginPageViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let authorizationAppleIDButton = ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
+        authorizationAppleIDButton.cornerRadius = 8
+        authorizationAppleIDButton.addTarget(self, action: #selector(pressSignInWithAppleButton), for: UIControl.Event.touchUpInside)
+        view.addSubview(authorizationAppleIDButton)
+        
+        authorizationAppleIDButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            authorizationAppleIDButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            authorizationAppleIDButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100)
+        ])
+    }
+    
+    
+//    // 淺色模式就顯示黑色的按鈕，深色模式就顯示白色的按鈕
+//    func chooseAppleButtonStyle() -> ASAuthorizationAppleIDButton.Style {
+//        return (UITraitCollection.current.userInterfaceStyle == .light) ?.black: .white
+//    }
+    
+    
+    // 點擊 Sign In with Apple 按鈕後，請求授權
+    @objc func pressSignInWithAppleButton() {
+        let authorizationAppleIDRequest: ASAuthorizationAppleIDRequest = ASAuthorizationAppleIDProvider().createRequest()
+        authorizationAppleIDRequest.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [authorizationAppleIDRequest])
+        
+        controller.delegate = self
+        controller.presentationContextProvider = self //告知 ASAuthorizationController 該呈現在哪個 Window 上
+        
+        controller.performRequests()
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension LoginPageViewController: ASAuthorizationControllerDelegate {
+    
+    // 授權成功
+    // - Parameters:
+    //   - controller: _
+    //   - authorization: _
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            print("user: \(appleIDCredential.user)") // Apple 唯一識別碼，該值在同一個開發者帳號下所有的 App 都會是一樣的
+            print("fullName: \(String(describing: appleIDCredential.fullName))")
+            print("Email: \(String(describing: appleIDCredential.email))")
+            print("realUserStatus: \(String(describing: appleIDCredential.realUserStatus))")
+            print("Authorization Code: \(String(describing: appleIDCredential.authorizationCode))")
+            print("Identity Token: \(String(describing: appleIDCredential.identityToken))")
+        }
     }
-    */
+    
+    // 授權失敗
+    // - Parameters:
+    //   - controller: _
+    //   - error: _
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        
+        switch (error) {
+        case ASAuthorizationError.canceled:
+            break
+        case ASAuthorizationError.failed:
+            break
+        case ASAuthorizationError.invalidResponse:
+            break
+        case ASAuthorizationError.notHandled:
+            break
+        case ASAuthorizationError.unknown:
+            break
+        default:
+            break
+        }
+        
+        print("didCompleteWithError: \(error.localizedDescription)")
+    }
+}
 
+
+//告知 ASAuthorizationController 該呈現在哪個 Window 上
+extension LoginPageViewController: ASAuthorizationControllerPresentationContextProviding {
+    // - Parameter controller: _
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return (self.view?.window)!
+    }
+    
 }
