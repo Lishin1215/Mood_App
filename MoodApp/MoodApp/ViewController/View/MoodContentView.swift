433db1a6b5dbaf83056b77cc01ff341102acd59d
Lishin <99582713+Lishin1215@users.noreply.github.com>
2023-07-04 14:27:14 +0800

    change the date format of xAsis in moodContentView

--- MoodApp/MoodApp/ViewController/View/MoodContentView.swift
+++ MoodApp/MoodApp/ViewController/View/MoodContentView.swift
@@ -21,6 +21,17 @@ struct MoodContentView: View {
     
     //畫圖需要的資料，moodFlow的array
     var moodFlowArray: [MoodFlow] = []
+    var startOfMonth: Date = Date() //月的第一天，作為x軸的頭
+    var endOfMonth: Date = Date() //月的最後一天
+    var monthLabelArray: [Date] = []
+    
+    //用來將date轉換成"xx/xx"的形式
+    let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM/dd"
+        return formatter
+    }()
+    
 
     //***把statisticsVC拿到的moodArray傳過來
     init(moodArray: [MoodFlow]) {
@@ -28,6 +39,15 @@ struct MoodContentView: View {
         
         // 將Array以日期排序，不然畫出來的圖會怪怪的（是按放的順序連線）
 //        moodFlowArray = moodFlowArray.sorted(by: { $0.date < $1.date })
+        
+        // 從moodFlowArray的Date中，去確認圖表要用到哪個月份
+        startOfMonth = moodFlowArray[0].date.startDateOfMonth //該月第一天
+        endOfMonth = moodFlowArray[0].date.endDateOfMonth //該月最後一天
+        
+        //從startOfMonth到endOfMonth為止，產生一個間隔為5天的array (3600秒 * 24小時 * 5天）
+        monthLabelArray = Array(stride(from: startOfMonth, to: endOfMonth, by: 3600 * 24 * 5))
+        
+    
     }
     
     
@@ -46,10 +66,30 @@ struct MoodContentView: View {
                 .foregroundStyle(Color(uiColor: .orangeBrown))
             }
         }
+        // y軸
         .chartYAxis {
             AxisMarks(position: .leading, values: [0,1,2,3,4]) //讓y軸固定5個值
         }
         .chartYScale(domain: 0...4) //限制y軸範圍
+        
+        // x軸
+        .chartXAxis {
+            
+            //利用monthLabelArray作為x軸label，並把date轉換成"xx/xx"的形式
+            AxisMarks(values: monthLabelArray) { value in
+                
+                if let date = value.as(Date.self) {
+                    AxisValueLabel {
+                        VStack(alignment: .leading) {
+                            Text(date, formatter: dateFormatter)
+                        }
+                    }
+                }
+                
+                AxisGridLine() //網格線
+                AxisTick() //標點
+            }
+        }
         
     }
 }
